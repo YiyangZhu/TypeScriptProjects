341c78f40e89c8e7554a58484f287ee3400b9e86
Yiyang Zhu <ererandkeke@gmail.com>
2019-01-24 22:31:05 -0500

    part 1 of module

--- MS_Course/Module3/namespaceAndModulesLabs/modulesLab/arrayUtils.ts
+++ MS_Course/Module3/namespaceAndModulesLabs/modulesLab/arrayUtils.ts
@@ -1,17 +1,19 @@
-namespace ArrayUtilities{
-    export function reverseArray(array: Array<number>){
+class ArrayUtilities{
+    reverseArray(array: Array<number>){
         return array.slice(0).reverse();
     }
 
-    export function lastItemOfArray(array: Array<number>){
+    lastItemOfArray(array: Array<number>){
         return array.slice(0).pop();
     }
 
-    export function firstItemOfArray(array: Array<number>){
+    firstItemOfArray(array: Array<number>){
         return array.slice(0).shift();
     }
 
-    export function concatenateArray(array1: Array<number>, array2: Array<number>){
+    concatenateArray(array1: Array<number>, array2: Array<number>){
         return array1.concat(array2);
     }
 }
+
+export default new ArrayUtilities;
