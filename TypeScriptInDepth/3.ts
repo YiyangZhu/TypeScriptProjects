77274bfbfd55760bccfd441a280de57df6c64e83
Yiyang Zhu <ererandkeke@gmail.com>
2019-03-06 14:02:39 -0500

    null value and this

---
+++ TypeScriptInDepth/3.ts
@@ -0,0 +1,30 @@
+console.log(undefined == undefined); //true
+
+console.log(null == undefined); //true
+
+console.log(0 == undefined); //false
+
+console.log('' == undefined); //false
+
+console.log(false == undefined); //false
+
+//recommend ==null to check for both undefined or null. 
+
+var someglobal;
+
+if(typeof someglobal !== 'undefined'){
+	console.log(someglobal);
+}
+
+function foo(){
+	console.log(this);
+}
+
+foo();
+let bar = {
+	foo
+}
+
+bar.foo();
+
+
