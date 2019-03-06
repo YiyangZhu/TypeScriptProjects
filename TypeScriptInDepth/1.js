c68f03ae48bdcbc24981ad8b9e285ec1e75d9ac6
Yiyang Zhu <ererandkeke@gmail.com>
2019-03-06 11:07:45 -0500

    ambient types, class

--- TypeScriptInDepth/1.js
+++ TypeScriptInDepth/1.js
@@ -8,6 +8,21 @@ var point3D = { x: 0, y: 10, z: 20 };
 function iTakePoint2D(point) {
     //do sth
 }
-iTakePoint2D(point2D);
-iTakePoint2D(point3D);
-iTakePoint2D({ x: 0 });
+iTakePoint2D(point2D); //exact match okay
+iTakePoint2D(point3D); //extra information okay
+$('.awesome').show();
+//$(123).show(); // error, selector needs to be a string, which is defined in declaration of var $
+var Point = /** @class */ (function () {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    Point.prototype.add = function (point) {
+        return new Point(this.x + point.x, this.y + point.y);
+    };
+    return Point;
+}());
+var p1 = new Point(0, 10);
+var p2 = new Point(10, 20);
+var p3 = p1.add(p2);
+var inc = function (x) { return x + 1; };
--- TypeScriptInDepth/1.ts
+++ TypeScriptInDepth/1.ts
@@ -26,4 +26,34 @@ function iTakePoint2D(point: Point2D){
 
 iTakePoint2D(point2D); //exact match okay
 iTakePoint2D(point3D); //extra information okay
-iTakePoint2D({x: 0}); //error: missing information 'y'
+//iTakePoint2D({x: 0}); //error: missing information 'y'
+
+//conclusion: extra information is okay, unsufficient parameter information is not allowed, introducing error
+
+//$('.awesome').show(); //Error: cannot find name '$'
+
+/*
+declare var $: any;
+$('.awesome').show(); //okay!
+*/
+
+declare var $: {
+	(selector: string): any;
+};
+$('.awesome').show();
+//$(123).show(); // error, selector needs to be a string, which is defined in declaration of var $
+
+class Point{
+	constructor(public x: number, public y: number){
+	}
+
+	add(point: Point){
+	return new Point(this.x + point.x,this.y + point.y);
+	}
+}
+
+var p1 = new Point(0,10);
+var p2 = new Point(10,20);
+var p3 = p1.add(p2);
+
+var inc = x => x + 1;
